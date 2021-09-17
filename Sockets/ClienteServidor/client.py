65435a6c9639ec368b84df0596840977470cffef
LeninFranco <90914859+LeninFranco@users.noreply.github.com>
2021-09-17 09:26:18 -0500

    Add files via upload

---
+++ Sockets/ClienteServidor/client.py
@@ -0,0 +1,38 @@
+import socket
+import json
+
+HEADER = 64
+PORT = 5050
+FORMAT = 'utf-8'
+DISCONNECT_MESSAGE = "!DISCONNECT"
+SERVER = socket.gethostbyname(socket.gethostname())
+ADDR = (SERVER, PORT)
+
+def send(msg):
+    if msg != "":
+        message = msg.encode(FORMAT)
+        msg_length = len(message)
+        send_length = str(msg_length).encode(FORMAT)
+        send_length += b' ' * (HEADER - len(send_length))
+        client.send(send_length)
+        client.send(message)
+        print(client.recv(2048).decode(FORMAT))
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect(ADDR)
+
+def readList():
+    msg = []
+    for i in range(3):
+        var = input("->")
+        if var == DISCONNECT_MESSAGE:
+            return var
+        msg.append(var)
+    return json.dumps(msg)
+
+while True:
+    msg = input("->")
+    send(msg)
+    if msg == DISCONNECT_MESSAGE:
+        client.close()
+        break
---
+++ Sockets/ClienteServidor/server.py
@@ -0,0 +1,43 @@
+import socket 
+import threading
+import json
+
+HEADER = 64
+PORT = 5050
+SERVER = socket.gethostbyname(socket.gethostname())
+ADDR = (SERVER, PORT)
+FORMAT = 'utf-8'
+DISCONNECT_MESSAGE = "!DISCONNECT"
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind(ADDR)
+
+def handle_client(conn, addr):
+    print(f"[NEW CONNECTION] {addr} connected.")
+    while True:
+        msg_length = conn.recv(HEADER).decode(FORMAT)
+        if msg_length:
+            msg_length = int(msg_length)
+            msg = conn.recv(msg_length).decode(FORMAT)
+            if msg == DISCONNECT_MESSAGE:
+                print(f"[{addr}] {msg}")
+                break
+            #msg = json.loads(msg)
+            #msg = list(map(lambda i: i.upper(),msg)) #Accion a la lista
+            print(f"[{addr}] {msg}")
+            #msg = json.dumps(msg)
+            conn.send(msg.upper().encode(FORMAT))
+    conn.close()
+
+def start():
+    server.listen()
+    print(f"[LISTENING] Server is listening on {SERVER}")
+    while True:
+        conn, addr = server.accept()
+        thread = threading.Thread(target=handle_client, args=(conn, addr))
+        thread.start()
+        print(f"[ACTIVE CONNECTIONS] {threading.activeCount() - 1}")
+
+
+print("[STARTING] server is starting...")
+start()
