afd58a0d7aad9fa48023cd4dad187985ae7f501b
LeninFranco <90914859+LeninFranco@users.noreply.github.com>
2021-09-26 13:51:41 -0500

    Update server.py

--- Sockets/ClienteServidor/server.py
+++ Sockets/ClienteServidor/server.py
@@ -7,7 +7,7 @@ PORT = 5050
 SERVER = socket.gethostbyname(socket.gethostname())
 ADDR = (SERVER, PORT)
 FORMAT = 'utf-8'
-DISCONNECT_MESSAGE = "!DISCONNECT"
+DISCONNECT_MESSAGE = "EXIT"
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind(ADDR)
