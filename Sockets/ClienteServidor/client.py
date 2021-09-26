199aa94c4bdb2935a8e09deb7058cb439208a5b9
LeninFranco <90914859+LeninFranco@users.noreply.github.com>
2021-09-26 13:50:38 -0500

    Update client.py

--- Sockets/ClienteServidor/client.py
+++ Sockets/ClienteServidor/client.py
@@ -4,9 +4,12 @@ import json
 HEADER = 64
 PORT = 5050
 FORMAT = 'utf-8'
-DISCONNECT_MESSAGE = "!DISCONNECT"
+DISCONNECT_MESSAGE = "EXIT"
 SERVER = socket.gethostbyname(socket.gethostname())
 ADDR = (SERVER, PORT)
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect(ADDR)
 
 def send(msg):
     if msg != "":
@@ -17,9 +20,6 @@ def send(msg):
         client.send(send_length)
         client.send(message)
         print(client.recv(2048).decode(FORMAT))
-
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect(ADDR)
 
 def readList():
     msg = []
