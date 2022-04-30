5a9948e64cd31025704312d703b9a07220c8a8e7
LeninFranco <90914859+LeninFranco@users.noreply.github.com>
2022-04-29 20:36:43 -0500

    Add files via upload

---
+++ Flask/Control Acceso/App.py
@@ -0,0 +1,168 @@
+from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
+import pymysql
+from flask import *
+from hashlib import md5
+import smtplib 
+from email.message import EmailMessage 
+
+app = Flask(__name__)
+
+app.secret_key = 'mykey'
+
+@app.route('/')
+def index():
+    if "username" in session:
+        return redirect(url_for('home'))
+    else:
+        return render_template('index.html')
+
+@app.route('/login', methods=['POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['user']
+        password = request.form['pass']
+        hashPassword = md5(password.encode())
+        conn = pymysql.connect(host='localhost', port=3306, user='root', password='P0L1m4s7er!', db='crypto')
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM usuarios WHERE nombreUsuario=%s AND passUsuario=%s", (username,hashPassword.hexdigest()))
+        fila = cursor.fetchone()
+        if fila == None:
+            flash('danger')
+            flash('Usuario o Contraseña incorrectos')
+            return redirect('/')
+        else:
+            session["username"] = username
+            return redirect('/home')
+
+@app.route('/logout')
+def logout():
+    session.clear()
+    return redirect('/')
+
+@app.route('/home')
+def home():
+    if "username" in session:
+        return render_template('home.html', usuario=session["username"])
+    else:
+        return redirect(url_for('index'))
+
+@app.route('/crearCuenta')
+def crearCuenta():
+    if "username" in session:
+        return redirect(url_for('home'))
+    else:
+        return render_template('signup.html')
+
+@app.route('/guardarCuenta', methods=['POST'])
+def guardarCuenta():
+    if request.method == 'POST':
+        username = request.form['user']
+        password = request.form['pass']
+        email = request.form['correo']
+        hashPassword = md5(password.encode())
+        conn = pymysql.connect(host='localhost', port=3306, user='root', password='P0L1m4s7er!', db='crypto')
+        cursor = conn.cursor()
+        cursor.execute("INSERT INTO usuarios(nombreUsuario,passUsuario,emailUsuario) VALUES(%s,%s,%s)",(username,hashPassword.hexdigest(),email))
+        conn.commit()
+        cursor.close()
+        conn.close()
+        flash('success')
+        flash('Cuenta Creada Correctamente')
+        return redirect('/')
+
+@app.route('/recuperarContrasena')
+def recuperarContrasena():
+    if "username" in session:
+        return redirect(url_for('home'))
+    else:
+        return render_template('reset.html')
+
+def getToken(userid):
+    serial = Serializer('mykey',300)
+    return serial.dumps({'user_id':userid}).decode('utf-8')
+
+def verifyToken(token):
+    serial = Serializer('mykey')
+    try:
+        user_id = serial.loads(token)['user_id']
+    except: 
+        return None
+    conn = pymysql.connect(host='localhost', port=3306, user='root', password='P0L1m4s7er!', db='crypto')
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM usuarios WHERE idUsuario=%s", (user_id,))
+    return cursor.fetchone()
+
+def sendMail(user):
+    token = getToken(user[0])
+    email_subject = "Recuperación de Contraseña" 
+    sender_email_address = "escomequipo1@gmail.com" 
+    receiver_email_address = user[3]
+    email_smtp = "smtp.gmail.com" 
+    email_password = "escombros" 
+
+    message = EmailMessage() 
+
+    message['Subject'] = email_subject 
+    message['From'] = sender_email_address 
+    message['To'] = receiver_email_address 
+
+    message.set_content(f'''Para crear una nueva contraseña, por favor de un clic al siguiente link:
+
+    {url_for('resetPassword',token=token,_external=True)} 
+    
+    Si no solicitaste una recuperación de contraseña, ignora este mensaje. ''') 
+
+    server = smtplib.SMTP(email_smtp, '587') 
+
+    server.ehlo() 
+
+    server.starttls() 
+
+    server.login(sender_email_address, email_password) 
+
+    server.send_message(message) 
+
+    server.quit()
+
+
+@app.route('/changePassword', methods=['POST'])        
+def changePassword():
+    if request.method == 'POST':
+        email = request.form['correo']
+        conn = pymysql.connect(host='localhost', port=3306, user='root', password='P0L1m4s7er!', db='crypto')
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM usuarios WHERE emailUsuario=%s", (email,))
+        fila = cursor.fetchone()
+        if fila == None:
+            flash('danger')
+            flash('No hay ningun usuario con ese correo')
+            return redirect('/recuperarContrasena')
+        else:
+            flash('success')
+            flash('Se ha enviado un correo para la recuperación de su contraseña')
+            sendMail(fila)
+            return redirect('/recuperarContrasena')
+
+@app.route('/changePassword/<token>')
+def resetPassword(token):
+    user = verifyToken(token)
+    if user == None:
+        return redirect(url_for('recuperarContrasena'))
+    else:
+        return render_template('changePassword.html')
+
+@app.route('/updatePassword', methods=['POST'])
+def updatePassword():
+    if request.method == 'POST':
+        password = request.form['pass']
+        hashPassword = md5(password.encode())
+        conn = pymysql.connect(host='localhost', port=3306, user='root', password='P0L1m4s7er!', db='crypto')
+        cursor = conn.cursor()
+        cursor.execute("UPDATE usuarios SET passUsuario=%s",(hashPassword.hexdigest(),))
+        conn.commit()
+        cursor.close()
+        conn.close()
+        return redirect('/')
+
+if __name__ == "__main__":
+    app.run(port=3000, debug=True)
